fb27bf97d0c21ef21b6b693b862ed40c8aee46f8
alefra88 <efra.rlz@gmail.com>
2023-05-18 06:11:07 -0600

    os

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,2 +1,20 @@
-console.log('Welcome to Node Tutorial')
-require("./07-mind-grenade")
+// console.log('Welcome to Node Tutorial')
+// require("./07-mind-grenade")
+
+const os = require("os")
+
+//info about current user
+
+const user = os.userInfo()
+console.log(user)
+
+console.log(`The system  Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+  name:os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+
+console.log(currentOS)
